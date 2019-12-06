e1392ca1463b4f4dc654c1c6efde095458a6f6f5
Robert Günzler <r@gnzler.io>
2019-12-06 17:13:05 +0100

    Improve toggl_dmenu script
    Try to use wofi, rofi and dmenu in that order.
    Namespace toggl token env var that is used.

--- toggl_dmenu.sh
+++ toggl_dmenu.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 bail() {
-    exec rofi -e "$@"
+    if [ -t 0 ]; then
+        echo "$@" 1>&2
+    else
+        exec swaynag --edge bottom --message "$@"
+    fi
     exit 1
 }
-rofi() {
-    exec rofi -markup -markup-rows -dmenu -p toggl -i "$@"
+dmenu() {
+    dmenu_prompt=" toggle${DMENU_PROMPT:-}"
+    if [ -x "$(command -v wofi)" ]; then
+        exec wofi --cache-file /dev/null --allow-markup --dmenu --prompt "$dmenu_prompt"
+    fi
+    if [ -x "$(command -v rofi)" ]; then
+        exec rofi -markup -dmenu -mesg "$dmenu_prompt"
+    fi
+    if [ -x "$(command -v dmenu)" ]; then
+        exec dmenu -mesg "$dmenu_prompt"
+    fi
+    bail "no dmenu command found, tried wofi, rofi and dmenu."
 }
 
 ACTIONS=(
@@ -14,47 +28,47 @@ ACTIONS=(
     "List timers"
 )
 select_action() {
-    for a in "${ACTIONS[@]}"; do echo "$a"; done | rofi
+    for a in "${ACTIONS[@]}"; do echo "$a"; done | dmenu
 }
 
 select_project() {
-    ret=`toggl -t $TOKEN projects ls | rofi -mesg "<i>select a project</i>" -selected-row 1 | cut -d' ' -f 1`
+    ret=$(toggl -t "$TOGGL_TOKEN" projects ls | DMENU_PROMPT=": <i>select a project</i>" dmenu | cut -d' ' -f 1)
     if [[ -z $ret ]]; then
         return
     fi
-    toggl -t $TOKEN -j projects ls | jq -r ".[] | select(.id==$ret) | .name"
+    toggl -t "$TOGGL_TOKEN" -j projects ls | jq -r ".[] | select(.id==$ret) | .name"
 }
 
-if [[ -z $TOKEN ]]; then
+if [ -z "$TOGGL_TOKEN" ]; then
     bail "no toggl api token"
 fi
 
-case `select_action` in
+case $(select_action) in
     "${ACTIONS[0]}") # start timer
-        p=`select_project`
+        p=$(select_project)
         if [[ -z $p ]]; then
             exit 1
         fi
-        d=`rofi -mesg "<i>enter a description</i>"`
-        ret=`toggl -t $TOKEN timer start --description "$d" --project "$p"`
+        d=$(DMENU_PROMPT=": <i>enter a description</i>" dmenu)
+        ret=$(toggl -t "$TOGGL_TOKEN" timer start --description "$d" --project "$p")
         if [[ $? -ne 0 ]]; then
             bail "$ret"
         fi
         ;;
     "${ACTIONS[1]}") # stop timer
-        ret=`toggl -t $TOKEN timers ls | tail -n +2 | cut -d' ' -f1`
+        ret=`toggl -t $TOGGL_TOKEN timers ls | tail -n +2 | cut -d' ' -f1`
         if [[ $? -ne 0 ]]; then
             bail "$ret"
         fi
         if [[ -z $ret ]]; then
             exit 1
         fi
-        ret=`toggl -t $TOKEN timer stop "$ret"`
+        ret=$(toggl -t "$TOGGL_TOKEN" timer stop "$ret")
         if [[ $? -ne 0 ]]; then
             bail "$ret"
         fi
         ;;
-    "${ACTIONS[2]}") # running timer
-        toggl -t $TOKEN timers ls -a | rofi -selected-row 1
+    "${ACTIONS[2]}") # list timers
+        toggl -t "$TOGGL_TOKEN" timers ls -a | dmenu
         ;;
 esac
